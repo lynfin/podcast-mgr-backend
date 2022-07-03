1f4c3ddda25ffbefb8a5e17ee55d5867c9365b06
Lyn <lynfinman@gmail.com>
2022-07-02 21:23:42 -0400

    wrapped client in app.get

--- app.js
+++ app.js
@@ -11,38 +11,39 @@ const client = Client({
   apiKey: process.env.LISTEN_API_KEY || null,
 });
 
-client
-  .search({
-    q: "elon musk",
-  })
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    if (error.response) {
-      switch (error.response.status) {
-        case 404:
-          // Endpoint not exist or podcast / episode not exist
-          break;
-        case 401:
-          // Wrong API key, or your account is suspended
-          break;
-        case 400:
-          // Invalid parameters
-          break;
-        case 500:
-          // Server-side error
-          break;
-        default:
-          // Unknown errors
-          break;
+app.get("/", (req, res) => {
+  client
+    .search({
+      q: "elon musk",
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      if (error.response) {
+        switch (error.response.status) {
+          case 404:
+            // Endpoint not exist or podcast / episode not exist
+            break;
+          case 401:
+            // Wrong API key, or your account is suspended
+            break;
+          case 400:
+            // Invalid parameters
+            break;
+          case 500:
+            // Server-side error
+            break;
+          default:
+            // Unknown errors
+            break;
+        }
+      } else {
+        // Failed to connect to Listen API servers
       }
-    } else {
-      // Failed to connect to Listen API servers
-    }
-    console.log(error);
-  });
-
+      console.log(error);
+    });
+});
 /*
 app.get("/", (req, res) => {
   // send a get request to root directory ('/' is this file (app.js))
